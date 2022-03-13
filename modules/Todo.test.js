ff7e64d6bd6ad7b8f36bfe9ca5801a6cca889d2c
Sirvelasque <velasquezwalter10@yahoo.es>
2022-03-12 22:20:15 -0600

    linter erros fix

--- modules/Todo.test.js
+++ modules/Todo.test.js
@@ -3,11 +3,9 @@
  */
 import update from './ToDoList.js';
 import Task from './tasks.js';
-import { isSymbol } from 'lodash';
 import status from './status.js';
 
 describe('TodoList Test add and remove', () => {
-
   document.body.innerHTML = '<section class="main">'
   + '  <div id="list_container">'
   + '    <ul id="list">'
@@ -15,7 +13,6 @@ describe('TodoList Test add and remove', () => {
   + '    </ul>'
   + '  </div>'
   + '</section>';
-
   it('Add Task', () => {
     update.addTask('homework');
     update.addTask('eat');
@@ -31,7 +28,6 @@ describe('TodoList Test add and remove', () => {
       expect(task.index).toBe(update.tasks[index].index);
     });
   });
-
   it('Remove Task', () => {
     update.addTask('run');
     expect(update.tasks[0].task).toBe('homework');
@@ -42,49 +38,37 @@ describe('TodoList Test add and remove', () => {
     const data = JSON.parse(localStorage.getItem('tasks'));
     const tempData = data.map((value) => new Task(value.completed, value.task, value.index));
     expect(tempData.length).toBe(update.tasks.length);
+  });
 });
-
-});
-
-describe("Todo list test, change and clear completed tasks", () => {
-
+describe('Todo list test, change and clear completed tasks', () => {
   it('Change second task', () => {
     const newTask = "spin";
     const oldTask = update.tasks[1].task;
-
     update.tasks.forEach((task) => {
       update.saveChanges(task, oldTask, newTask);
-    })
-
+    });
     expect(update.tasks[1].task).toBe('spin');
   });
-
   it('Change third task', () => {
     const newTask = "laundry";
     const oldTask = update.tasks[2].task;
-
     update.tasks.forEach((task) => {
       update.saveChanges(task, oldTask, newTask);
-    })
-
+    });
     expect(update.tasks[2].task).toBe('laundry');
   });
-
   it('Completed status', () => {
     const task = update.tasks[0];
     task.completed = status.istrue();
     expect(task.completed).toBe(true);
-
-  })
-
+  });
   it('Remove completed from list', () => {
     update.tasks[1].completed = true
     update.tasks[2].completed = true
-
     update.removefromUI;
     const data = JSON.parse(localStorage.getItem('tasks'));
     const temp = data.map((value) => new Task(value.completed, value.task, value.index));
     expect(temp.length).toBe(update.tasks.length);
     expect(update.tasks.length).toBe(3);
-  })
+  });
 });
